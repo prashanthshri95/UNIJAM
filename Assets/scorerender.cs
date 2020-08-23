a4f6c075c9d6e6e28d88096894362bf217f01851
Cindy <cindy1.liu@hotmail.com>
2020-08-23 21:35:55 +1000

    updated score to display on end screens

---
+++ Assets/scorerender.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scorerender : MonoBehaviour
+{
+    public Text scoreUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreUI.text = PlayerPrefs.GetInt("Score",0).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
